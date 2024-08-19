7c6e8cea883d847edf7fdd7baedcfa7df60be50f
AramirRp <antoinerp@gmail.com>
2024-08-19 12:44:17 +0200

    allez là

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,26 +1,17 @@
 <?php
-<<<<<<< HEAD
+
 namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\User;
-=======
 
-namespace Database\Seeders;
-
-use Illuminate\Database\Seeder;
-use App\Models\User;
-
->>>>>>> bcedfec04e8e64a693057a8d342e610b12dd9133
 class UserSeeder extends Seeder
 {
     public function run()
     {
-<<<<<<< HEAD
-        // Crée 50 utilisateurs fictifs
-        User::factory()->count(50)->create();
-=======
+
         User::factory()->count(50)->create();
 
->>>>>>> bcedfec04e8e64a693057a8d342e610b12dd9133
+
     }
 }
+
