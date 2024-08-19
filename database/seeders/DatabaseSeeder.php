ab3392429581e1069545f278b00ee02c15ddcb90
AramirRp <antoinerp@gmail.com>
2024-08-19 12:46:50 +0200

    allez là

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,17 +8,9 @@ class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-<<<<<<< HEAD
-        $this->call([
-            UserSeeder::class,
-        ]);
-        $this->call([
-            PostSeeder::class,
-        ]);
-=======
+
         $this->call(UserSeeder::class);
         $this->call(PostSeeder::class);
 
->>>>>>> bcedfec04e8e64a693057a8d342e610b12dd9133
     }
 }
